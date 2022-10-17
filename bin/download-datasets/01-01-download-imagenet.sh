1996f8855ae56500b2bc1f41a26017c31120058b
Alexander Isenko <alex.isenko@protonmail.com>
2022-10-17 17:56:40 +0200

    changed imagenet and cube++ urls to use small datasets

--- bin/download-datasets/01-01-download-imagenet.sh
+++ bin/download-datasets/01-01-download-imagenet.sh
@@ -4,26 +4,28 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 echo "---"
-echo "${bold}1.1.1 Downloading Imagenet...${normal} (~ 30min)" 
+echo "${bold}1.1.1 Downloading Imagenet...${normal}" 
 echo "---"
-read -p "Docker is installed and available without sudo? ${bold}Confim.${normal}" choice
-read -p "Kaggle credentials (kaggle.json) are in ${bold}(...)/presto/bin/kaggle.json? Confirm.${normal}" choice
-echo "---"
-echo "Starting docker container to download the
-https://www.kaggle.com/competitions/imagenet-object-localization-challenge/data dataset"
+# read -p "Docker is installed and available without sudo? ${bold}Confim.${normal}" choice
+#read -p "Kaggle credentials (kaggle.json) are in ${bold}(...)/presto/bin/kaggle.json? Confirm.${normal}" choice
+#echo "---"
+#echo "Starting docker container to download the https://www.kaggle.com/competitions/imagenet-object-localization-challenge/data dataset"
 
-cp kaggle.json ./download-datasets/imagenet-docker/kaggle.json
-cd ./download-datasets/imagenet-docker/
-docker build -t kaggle-imagenet-downloader:v1 .
-docker run -v $IMAGENET_DATASET_PATH:/tmp/ kaggle-imagenet-downloader:v1 
+# cp kaggle.json ./download-datasets/imagenet-docker/kaggle.json
+#cd ./download-datasets/imagenet-docker/
+#docker build -t kaggle-imagenet-downloader:v1 .
+#docker run -v $IMAGENET_DATASET_PATH:/tmp/ kaggle-imagenet-downloader:v1 
 
-echo "${bold}1.1.2 Extracting...${normal} (~ 1-2h)"
+#echo "${bold}1.1.2 Extracting...${normal} (~ 1-2h)"
+URL="https://syncandshare.lrz.de/dl/fiWHsi4sgHC24UGEAwUYFC/imagenet-small-isenko-v2.zip"
+
+wget -q --show-progress ${URL} --no-check-certificate -P ${IMAGENET_DATASET_PATH}
 
 cd ${IMAGENET_DATASET_PATH}
-unzip imagenet-object-localization-challenge.zip
+unzip imagenet-small-isenko-v2.zip
 echo "${bold}-- Finished extraction!"
 
 
 echo "${bold}1.1.4 Removing compressed files...${normal}"
-rm imagenet-object-localization-challenge.zip
+rm imagenet-small-isenko-v2.zip
 echo "${bold}-- Finished removing compressed dataset!${normal}"
--- bin/download-datasets/01-02-download-cube++.sh
+++ bin/download-datasets/01-02-download-cube++.sh
@@ -3,7 +3,7 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-URL_JPG="https://zenodo.org/record/4153431/files/JPG.zip"
+URL_JPG="https://syncandshare.lrz.de/dl/fiGHVKBA6x3RbGaYZSfPWo/cube%2B%2B-small-isenko.zip"
 
 echo "${bold}1.2.1 Downloading Cube++ dataset..."
 echo "From: ${URL}"
@@ -15,37 +15,38 @@ wget -q --show-progress ${URL_JPG} --no-check-certificate -P ${CUBEPP_DATASET_PA
 
 # downloading the PNG archives
 # they forgot to upload the PNG0 archive on zenodo
-wget -q --show-progress https://storage.yandexcloud.net/cubepng0/PNG0.zip
+#wget -q --show-progress https://storage.yandexcloud.net/cubepng0/PNG0.zip --no-check-certificate -P ${CUBEPP_DATASET_PATH}
 
-for ID in 1 2 3 4 5 6 7 8 9; do
-  URL="https://zenodo.org/record/4153431/files/PNG${ID}.zip"
-  wget -q --show-progress ${URL} --no-check-certificate -P ${CUBEPP_DATASET_PATH}
-done
+#for ID in 1 2 3 4 5 6 7 8 9; do
+#  URL="https://zenodo.org/record/4153431/files/PNG${ID}.zip"
+#  wget -q --show-progress ${URL} --no-check-certificate -P ${CUBEPP_DATASET_PATH}
+#done
 
 echo "${bold}--- Finished downloading!"
 echo "1.2.2 Extracting...${normal}"
 
 cd ${CUBEPP_DATASET_PATH}
-unzip JPG.zip
-for ID in 0 1 2 3 4 5 6 7 8 9; do
-  echo "Extracting ./PNG${ID}/Cube++/PNG to ./PNG"
-  unzip PNG${ID}.zip -d PNG${ID}
-done
+unzip cube++-small-isenko.zip
+#for ID in 0; do
+#  echo "Extracting ./PNG${ID}/Cube++/PNG to ./PNG"
+#  unzip PNG${ID}.zip -d PNG${ID}
+#done
 echo "${bold}-- Finished extraction!"
 
 echo "1.2.3 Removing compressed file...${normal}"
-rm JPG.zip PNG*.zip
+#rm JPG.zip PNG*.zip
+rm *.zip
 echo "${bold}-- Finished removing compressed dataset!${normal}"
 
-echo "${bold}1.2.4 Moving all .jpg/.png files in the corresponding directories...${normal}"
+#echo "${bold}1.2.4 Moving all .jpg/.png files in the corresponding directories...${normal}"
 
-mkdir -p JPG
-mv ./Cube++/JPG/*.jpg JPG/
-rm -r ./Cube++
+#mkdir -p JPG
+#mv ./Cube++/JPG/*.jpg JPG/
+#rm -r ./Cube++
 
-mkdir -p PNG
-for ID in 0 1 2 3 4 5 6 7 8 9; do
-  mv ./PNG${ID}/Cube++/PNG/*.png ./PNG/
-  rm -r ./PNG${ID}
-done
-echo "${bold}-- Finished moving all .jpg/.png files!${normal}"
+#mkdir -p PNG
+#for ID in 0; do
+#  mv ./PNG${ID}/Cube++/PNG/*.png ./PNG/
+#  rm -r ./PNG${ID}
+#done
+#echo "${bold}-- Finished moving all .jpg/.png files!${normal}"
